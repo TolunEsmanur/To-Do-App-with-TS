f602fa70d1172ac291392abd9a32a151ea5dfde3
TolunEsmanur <160126791+TolunEsmanur@users.noreply.github.com>
2024-10-24 10:50:14 +0300

    style.css

--- todo-app/todo-app/src/components/TodoList.tsx
+++ todo-app/todo-app/src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import { Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import TodoItem from "./TodoItem";
+import  "./style.css"
 
 interface ITodoList {
   todos:ITodoType[]
