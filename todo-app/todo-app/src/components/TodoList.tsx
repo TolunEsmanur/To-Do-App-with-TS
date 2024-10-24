753f9ddb55cbe599a153091cddb03cf8a3f71c7b
TolunEsmanur <160126791+TolunEsmanur@users.noreply.github.com>
2024-10-24 10:47:27 +0300

    TodoList

--- todo-app/todo-app/src/components/TodoList.tsx
+++ todo-app/todo-app/src/components/TodoList.tsx
@@ -1,11 +1,14 @@
 import { Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
+import TodoItem from "./TodoItem";
 
 interface ITodoList {
   todos:ITodoType[]
 }
 
 const TodoList: React.FC<ITodoList> = ({todos}) => {
+  const inProgressTodos = todos.filter(todo=>!todo.isDone)
+  const completedTodos = todos.filter(todo=>  todo.isDone)
   return (
     <Grid
       container
@@ -29,7 +32,31 @@ const TodoList: React.FC<ITodoList> = ({todos}) => {
           borderRadius: "0.5rem",
         }}
       >
-        <Typography>InProgress Todos</Typography>
+        <Typography className="title" color="secondary"
+        align="center" variant="h4">InProgress Todos</Typography>
+        {
+          inProgressTodos.length ? inProgressTodos.map(todo=><TodoItem key={todo.id} todo={todo} />) : 
+          <Typography color="error" mt={3}>No InProgress Todos</Typography>
+        }
+      </Grid>
+      <Grid
+        size={{ xs: 12, sm: 8, md: 5 }}
+        position="relative"
+        className="myscrool scrool-progress"
+        sx={{
+          minHeight: "350px",
+          maxHeight: "350px",
+          overflow: "auto",
+          border: "1px solid purple",
+          borderRadius: "0.5rem",
+        }}
+      >
+        <Typography className="title" sx={{color:"green"}}
+        align="center" variant="h4">Completed Todos</Typography>
+        {
+          completedTodos.length ? completedTodos.map(todo=><TodoItem key={todo.id} todo={todo} />) : 
+          <Typography color="error" mt={3}>No Completed Todos!</Typography>
+        }
       </Grid>
     </Grid>
   )
